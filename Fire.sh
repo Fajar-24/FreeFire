6ee44d90480cb71f138114d6d7c791b53dd8d8aa
https://github.com/Fajar-24/DARKFB <65275087+Fajar-24@users.noreply.github.com>
2020-06-05 03:11:25 +0700

    Create Fire.sh

---
+++ Fire.sh
@@ -0,0 +1,41 @@
+GNU nano 4.6               x.sh
+#Jangan Di Recode Ya Todd!!
+#!/usr/bin/sh
+# coding=utf-8
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+figlet "HACK FF"|lolcat
+echo "#######################################"|lolcat
+echo  AUTHOR  : FAJAR CYBER|lolcat
+echo  WHATSAP : 08817805411|lolcat
+echo  GITHUB  : GITHUB.COM/Fajar-24|lolcat
+echo "#######################################"|lolcat
+echo   "SELAMAT DATANG SEMOGA HARI MU MENYENANGKAN"|lolcat
+echo
+read -p "# MASUKAN ID TARGET FREE FIRE : " ID:
+echo "*TUNGGU SEBENTAR SEDANG MEMBUKA KEAMANAN"|lolcat
+echo "[✓]TUNGGU SEBENTAR..."|lolcat
+echo "[✓]MEMBUKA KEAMANAN..."|lolcat
+echo "[✓]MEMBUKA PASSWORD..."|lolcat
+echo "[✓]BERHASIL ✓ "|lolcat
+echo "----------------------------"|lolcat
+echo " [✓] PASSWORD :********"|lolcat
+figlet "EROR"|lolcat
+echo "KARENA ANDA BANYAK DOSA GOBLOK ASU"|lolcat
+echo "WHAT THE FUCK"|lolcat
+figlet "anjing"|lolcat
+figlet "FUCK"|lolcat
